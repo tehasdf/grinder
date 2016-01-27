3795a5dcff3df0eae3639bdd3dfc90eb4d639547
asdf <tehasdf@gmail.com>
2016-01-27 23:40:31 +0100

    use webpack for the building

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry: {
+        main: "./src/main.js"
+    },
+    output: {
+        path: './dist',
+        filename: "[name].bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" },
+            { test: /\.js$/, exclude: /node_modules/, loaders: ["babel?presets[]=es2015&presets[]=react&presets[]=stage-0"]}
+        ]
+    },
+    devServer: {
+        contentBase: './dist',
+        historyApiFallback: true
+    }
+};
